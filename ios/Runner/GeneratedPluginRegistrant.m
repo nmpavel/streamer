382dd80a8f6cf45f3ec337851e05f9463a2295dd
nmpavel <53833983+nmpavel@users.noreply.github.com>
2022-08-20 15:27:32 +0600

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<webview_media/FLTWebViewFlutterPlugin.h>)
+#import <webview_media/FLTWebViewFlutterPlugin.h>
+#else
+@import webview_media;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTWebViewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTWebViewFlutterPlugin"]];
+}
+
+@end
---
+++ lib/utilities/keys.dart
@@ -0,0 +1,1 @@
+const String API_KEY = 'AIzaSyBUlUHHi8flpPKF8-4Md0GuGMRv3jT91k0';
